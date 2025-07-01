c824cf835d00865a41ff9a659a9ed943253d59a1
syifaamdh <syifaamdh@gmail.com>
2025-07-01 16:39:39 +0700

    Create 2025-07-01-061229_Unit.php

---
+++ app/Database/Migrations/2025-07-01-061229_Unit.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Unit extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 10,
+                'unsigned' => true,
+                'auto_increment' => true,
+                'null'       => false,
+            ],
+            'parent_id' => [
+                'type' => 'SMALLINT',
+                'constraint' => 4,
+                'null'       => false,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 40,
+                'null'       => false,
+            ],
+            'status' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'null'       => false,
+                'comment' => '1=Active, 2=Inactive'
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('unit');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('unit');
+    }
+}
