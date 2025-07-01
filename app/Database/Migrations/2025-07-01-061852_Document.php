9487ddcfab6de14a6f0f057f601f24c45dc40580
syifaamdh <syifaamdh@gmail.com>
2025-07-01 16:39:51 +0700

    Create 2025-07-01-061852_Document.php

---
+++ app/Database/Migrations/2025-07-01-061852_Document.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Document extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'auto_increment' => true,
+                'null'           => false,
+            ],
+            'type' => [
+                'type'       => 'SMALLINT',
+                'constraint' => 4,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'number' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 40,
+                'null'       => false,
+            ],
+            'date_published' => [
+                'type' => 'DATE',
+                'null' => false,
+            ],
+            'revision' => [
+                'type'       => 'CHAR',
+                'constraint' => 3,
+                'null'       => false,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => false,
+            ],
+            'description' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 200,
+                'null'       => true,
+            ],
+            'filepath' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 200,
+                'null'       => false,
+            ],
+            'unit_id' => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'status' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'null'       => false,
+                'comment'    => '0=Not Approved, 1=Approved, 2=Disapproved',
+            ],
+            'createddate' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+                'default' => null,
+            ],
+            'createdby' => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true); 
+        $this->forge->createTable('document');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('document');
+    }
+}
