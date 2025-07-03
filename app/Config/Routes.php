a902edc7697c1bd739164c31b061f381f6400636
syifaamdh <syifaamdh@gmail.com>
2025-07-03 15:05:13 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,10 +13,6 @@ $routes->get('create-user/list', 'CreateUser::list');
 $routes->get('create-user/create', 'CreateUser::create');
 $routes->get('CreateUser/create', 'CreateUser::create');
 $routes->post('CreateUser/store', 'CreateUser::store');
-
-$routes->get('KelolaDokumen/getUserUnitData', 'KelolaDokumen::getUserUnitData');
-
-
 // DataMaster
 $routes->get('data-master', 'DataMaster::index');
 $routes->get('data-master/list', 'DataMaster::list');
@@ -25,8 +21,6 @@ $routes->get('data-master/create', 'DataMaster::create');
 // KelolaDokumen 
 $routes->get('dokumen/add', 'KelolaDokumen::add');
 $routes->get('dokumen/pengajuan', 'KelolaDokumen::pengajuan');
-$routes->get('dokumen/edit', 'KelolaDokumen::edit');
-
 
 // DaftarDokumen 
 $routes->get('dokumen/daftar', 'DaftarDokumen::index');
