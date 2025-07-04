ef61fdd231e7670223bb4ea098bfafbd4ae0efbd
syifaamdh <syifaamdh@gmail.com>
2025-07-04 11:16:12 +0700

    Create 2025-07-04-040805_DocumentRevision.php

---
+++ app/Database/Migrations/2025-07-04-040805_DocumentRevision.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class DocumentRevision extends Migration
+{
+     public function up()
+    {
+        $this->forge->addField([
+            'id'           => [
+                'type'           => 'INT',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'auto_increment' => true,
+                'null'           => false,
+            ],
+            'document_id'   => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'revision'      => [
+                'type'       => 'CHAR',
+                'constraint' => 3,
+                'null'       => false,
+            ],
+            'filename'      => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => false,
+            ],
+            'filepath'      => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => false,
+            ],
+            'filesize'      => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'remark'        => [
+                'type'       => 'VARCHAR',
+                'constraint' => 200,
+                'null'       => true,
+            ],
+            'createddate'   => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'createdby'     => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true); 
+        $this->forge->createTable('document_revision');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('document_revision');
+    }
+}
