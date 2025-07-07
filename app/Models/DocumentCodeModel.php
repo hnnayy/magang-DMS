ffc6b7b21ca20f82965073100242df69e215399f
syifaamdh <syifaamdh@gmail.com>
2025-07-07 15:27:37 +0700

    Create DocumentCodeModel.php

---
+++ app/Models/DocumentCodeModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class DocumentCodeModel extends Model
+{
+    protected $table = 'kode_dokumen';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $allowedFields = [
+        'document_type_id', 
+        'kode',
+        'nama',
+        'status',
+        'created_at',
+        'updated_at',
+    ];
+    protected $useTimestamps = true;
+}
