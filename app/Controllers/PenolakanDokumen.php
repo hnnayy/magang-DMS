e39bda3dc934d8755fc40f08b6aea7b5ab96933c
kinaya18 <152212592+kinaya18@users.noreply.github.com>
2025-07-17 13:49:47 +0700

    page-penolakan

---
+++ app/Controllers/PenolakanDokumen.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class PenolakanDokumen extends BaseController
+{
+    public function index()
+    {
+        return view('KelolaDokumen/daftar-penolakan');
+    }
+}
