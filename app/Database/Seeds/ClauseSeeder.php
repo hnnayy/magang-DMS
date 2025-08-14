4f6c14a88092c06a6b10962ce9078c1e71c7217e
hnnayy <mutiahanin2017@gmail.com>
2025-08-14 09:32:16 +0700

    clause dan standar seeder

--- app/Database/Seeds/ClauseSeeder.php
+++ app/Database/Seeds/ClauseSeeder.php
@@ -4,39 +4,34 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class ClauseSeeder extends Seeder
+class ClausesSeeder extends Seeder
 {
     public function run()
     {
-        // Ambil ID dari tabel standards
-        $standardModel = new \App\Models\StandardModel();
-        $standards = $standardModel->findAll();
+        $data = [
+            [
+                'id' => 1,
+                'standar_id' => 1,
+                'nama_klausul' => '7.1',
+                'description' => 'Resources',
+                'status' => 1
+            ],
+            [
+                'id' => 2,
+                'standar_id' => 1,
+                'nama_klausul' => '8.2.2',
+                'description' => 'Determining the requirements',
+                'status' => 1
+            ],
+            [
+                'id' => 4,
+                'standar_id' => 10,
+                'nama_klausul' => '7.1 tambah baruxx',
+                'description' => 'ini baruxx',
+                'status' => 1
+            ]
+        ];
 
-        $clauses = [];
-
-        foreach ($standards as $standard) {
-            if ($standard['nama_standar'] === 'ISO 9001') {
-                $clauses[] = [
-                    'standar_id'     => $standard['id'],
-                    'nomor_klausul'  => '7.1',
-                    'nama_klausul'   => 'Resources',
-                ];
-                $clauses[] = [
-                    'standar_id'     => $standard['id'],
-                    'nomor_klausul'  => '8.2.2',
-                    'nama_klausul'   => 'Determining the requirements',
-                ];
-            }
-
-            if ($standard['nama_standar'] === 'ISO 27001') {
-                $clauses[] = [
-                    'standar_id'     => $standard['id'],
-                    'nomor_klausul'  => '9.1',
-                    'nama_klausul'   => 'Monitoring, measurement, analysis and evaluation',
-                ];
-            }
-        }
-
-        $this->db->table('clauses')->insertBatch($clauses);
+        $this->db->table('clauses')->insertBatch($data);
     }
 }
--- app/Database/Seeds/StandardSeeder.php
+++ app/Database/Seeds/StandardSeeder.php
@@ -4,13 +4,35 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class StandardSeeder extends Seeder
+class StandardsSeeder extends Seeder
 {
     public function run()
     {
         $data = [
-            ['nama_standar' => 'ISO 9001'],
-            ['nama_standar' => 'ISO 27001'],
+            [
+                'id' => 1,
+                'nama_standar' => 'ISO 9001',
+                'description' => null,
+                'status' => 1
+            ],
+            [
+                'id' => 3,
+                'nama_standar' => 'tambah baru',
+                'description' => null,
+                'status' => 1
+            ],
+            [
+                'id' => 4,
+                'nama_standar' => 'sapi',
+                'description' => null,
+                'status' => 0
+            ],
+            [
+                'id' => 5,
+                'nama_standar' => 'standar baru 2',
+                'description' => 'deskripsi uv',
+                'status' => 0
+            ]
         ];
 
         $this->db->table('standards')->insertBatch($data);
